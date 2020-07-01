243d947d39a907e8db4187c736ce78eaad9c23e3
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-07-01 22:01:51 +0200

    Editing listboards

--- trainee_dashboard/templatetags/trainee_dashboard_extras.py
+++ trainee_dashboard/templatetags/trainee_dashboard_extras.py
@@ -8,7 +8,6 @@ register = template.Library()
 def screening_button(model_wrapper):
     title = ['Add subject\' screening form.']
     return dict(
-        subject_identifier=model_wrapper.object.subject_identifier,
         add_subject_screening_href=model_wrapper.subject_screening.href,
         subject_screening_model_obj=model_wrapper.subject_screening_model_obj,
         title=' '.join(title))
