50357e4a35eb51b5244ca48390227df8a9f15ae5
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-07-02 08:09:23 +0200

    Refactor Dashboard_extras

--- trainee_dashboard/templatetags/trainee_dashboard_extras.py
+++ trainee_dashboard/templatetags/trainee_dashboard_extras.py
@@ -8,6 +8,7 @@ register = template.Library()
 def screening_button(model_wrapper):
     title = ['Add subject\' screening form.']
     return dict(
+        screening_identifier=model_wrapper.object.screening_identifier,
         add_subject_screening_href=model_wrapper.subject_screening.href,
         subject_screening_model_obj=model_wrapper.subject_screening_model_obj,
         title=' '.join(title))
